4872614ed495560629279ec2aaf82fa0580af934
Ivan Mariic <imariic77@gmail.com>
2022-07-30 18:01:31 +0200

    feat: Added a few tests.

--- src/tests/PostsPage.test.js
+++ src/tests/PostsPage.test.js
@@ -1,5 +1,6 @@
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import { BrowserRouter as Router } from "react-router-dom";
+import App from "App";
 import { PostsPage } from "pages";
 
 describe("PostsPage", () => {
@@ -13,5 +14,37 @@ describe("PostsPage", () => {
     const search = screen.getByPlaceholderText(/Enter a post title/i);
 
     expect(search).toBeDefined();
+  });
+
+  it("checks if posts have been downloaded", async () => {
+    render(
+      <Router>
+        <App />
+      </Router>
+    );
+    const text = await screen.findByText(
+      /sunt aut facere repellat provident occaecati excepturi optio reprehenderit/i
+    );
+    expect(text).toBeDefined();
+  });
+
+  it("checks if filtering works", async () => {
+    render(
+      <Router>
+        <App />
+      </Router>
+    );
+
+    const filter = await screen.findByPlaceholderText(/Enter a post title/i);
+    const inputValue = "laborum non";
+    fireEvent.change(filter, {
+      target: {
+        value: inputValue,
+      },
+    });
+
+    expect(filter.value).toBe(inputValue);
+    const element = await screen.findAllByText("Name: Chelsey Dietrich");
+    expect(element).toBeDefined();
   });
 });
